152747102d923df3870043b6173e774f0a9add49
andresguanov <andresguanov@gmail.com>
2023-11-04 11:44:19 -0500

    fix: img Next problem to picture tag

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,11 +8,13 @@ export function Header () {
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <Link href="/">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
+                <picture>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt=""
+                  />
+                </picture>
               </Link>
             </a>
           </div>
